4164d6b701d09268ceed239b354cac0d77da14ee
Maripia Kuti <pionste@gmail.com>
2020-01-25 20:57:15 +0000

    connected to mongodb correctly and created test function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 import os
-from flask_pymongo import Pymongo
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 from os import path
@@ -10,18 +10,31 @@ if path.exists("env.py"):  # import file where username and password of MongoDB 
 app = Flask(__name__)  # create instance of flask
 
 # add configuration to Flask app
-app.config("MONGODB_NAME") = task_manager
-app.config("MONGOURL") = os.environ.get("MONGO_URL")
+MONGO_URI = os.environ["MONGODB_URI"]
+app.config["MONGODB_NAME"] = "task_manager"
+app.config["MONGO_URI"] = MONGO_URI
 
-mongo = Pymongo(app)  # create an instance of Pymongo with app object being pushed as argument
+mongo = PyMongo(app)  # create an instance of Pymongo with app object being pushed as argument
+
+
+'''def mongo_connect(url):  # function to connect to Mongo with url argument passed in
+    try:   # start try/except block
+        conn = pymongo.MongoClient(url)  # variable to connect to mongo client db
+        return conn  # return variable for connection
+    except pymongo.errors.ConnectionFailure as e:  # catch error if connection fails
+        print("Could not connect to MongoDB: %s") % e  # print error message which has been defined as 'e' alias'''
 
 
 @app.route("/")
-def test():
-    return "Hello World!"
+@app.route("/get_tasks")  # new route decorator to redirect to a particular function for the default view of the app
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())  # return everything in the db collection
 
 
 if __name__ == "__main__":
     app.run(host=os.getenv("IP"),
             port=os.getenv("PORT"), 
             debug=True)
+
+
+# conn = mongo_connect(MONGO_URI)  # calling function with MONGOBDB url
