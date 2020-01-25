437903b67f29a9f1b33381f31491f38a4a22446d
Maripia Kuti <pionste@gmail.com>
2020-01-25 18:42:44 +0000

    added more programs installed

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
 import os
+from flask_pymongo import Pymongo
+from bson.objectid import ObjectId
 
-app = Flask(__name__)
+from os import path
+if path.exists("env.py"):  # import file where username and password of MongoDB is saved
+    import env
+
+app = Flask(__name__)  # create instance of flask
+
+# add configuration to Flask app
+app.config("MONGODB_NAME") = task_manager
+app.config("MONGOURL") = os.environ.get("MONGO_URL")
+
+mongo = Pymongo(app)  # create an instance of Pymongo with app object being pushed as argument
 
 
 @app.route("/")
