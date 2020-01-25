80ada01671eb33fa48a55ea92fc8ea4023b964e4
Maripia Kuti <pionste@gmail.com>
2020-01-25 21:59:51 +0000

    added base.html and linked it to child html file

--- app.py
+++ app.py
@@ -17,14 +17,6 @@ app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)  # create an instance of Pymongo with app object being pushed as argument
 
 
-'''def mongo_connect(url):  # function to connect to Mongo with url argument passed in
-    try:   # start try/except block
-        conn = pymongo.MongoClient(url)  # variable to connect to mongo client db
-        return conn  # return variable for connection
-    except pymongo.errors.ConnectionFailure as e:  # catch error if connection fails
-        print("Could not connect to MongoDB: %s") % e  # print error message which has been defined as 'e' alias'''
-
-
 @app.route("/")
 @app.route("/get_tasks")  # new route decorator to redirect to a particular function for the default view of the app
 def get_tasks():
@@ -35,6 +27,3 @@ if __name__ == "__main__":
     app.run(host=os.getenv("IP"),
             port=os.getenv("PORT"), 
             debug=True)
-
-
-# conn = mongo_connect(MONGO_URI)  # calling function with MONGOBDB url
